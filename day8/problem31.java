21151855079cfdd51ed8b4528aad880aa4ef5d73
visionbypawan <glbitm3@gmail.com>
2026-06-03 22:23:31 +0530

    Added day7 folder

---
+++ day8/problem31.java
@@ -0,0 +1,11 @@
+public class problem31{
+    public static void main(String[] args) {
+        for(char i='A';i<='E';i++){
+            for(char j='A';j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    
+    }
+} 
