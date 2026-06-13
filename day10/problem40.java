016df8258d4c695275cb2d32de9c50d49c1d5b36
visionbypawan <glbitm3@gmail.com>
2026-06-13 09:42:36 +0530

    Added day10 folder

--- day10/problem40.java
+++ day10/problem40.java
@@ -2,16 +2,22 @@ public class problem40{
     public static void main(String[] args) {
         
         for(int i=1;i<=5;i++){
+            char ch='A';
+            
             for(int j=1;j<5-i;j++){
                 System.out.print(" ");
             }
             for(int j=1;j<=i;j++){
-                System.out.print();
+                System.out.print(ch);
+                ch++;
                 
             
             }
+            ch--;
             for(int j=i-1;j>0;j--){
-                System.out.print();
+                ch--;
+                System.out.print(ch);
+                
             }
             System.out.println();
             
